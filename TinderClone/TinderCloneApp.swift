b35e1262e01e1c07a99c6a07752201d621edb74a
ichichavez <isidrochavez429@gmail.com>
2024-03-30 00:15:24 -0400

    Initial Commit

---
+++ TinderClone/TinderCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinderCloneApp.swift
+//  TinderClone
+//
+//  Created by Isidro Chávez on 2024-03-30.
+//
+
+import SwiftUI
+
+@main
+struct TinderCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
